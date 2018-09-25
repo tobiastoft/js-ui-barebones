574faba03efde4a1b35820ec2dca99db584a3046
Tobias Toft <ttoft@ttoft-0M8HTD6.dyn.pinadmin.com>
2018-09-24 20:59:24 -0700

    Hmm

---
+++ server.js
@@ -0,0 +1,13 @@
+var connect  = require('connect');
+var static = require('serve-static');
+var server = connect();
+var port = 5555;
+
+server.use(  static(__dirname + '/src'));
+server.listen(port);
+
+var livereload = require('livereload');
+var lrserver = livereload.createServer();
+lrserver.watch(__dirname + "/src");
+
+console.log('Running on: http://localhost:' + port)
