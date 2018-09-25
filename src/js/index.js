8837ab6903bec28b3700e6dbaac868eabd360569
Tobias Toft <ttoft@ttoft-0M8HTD6.dyn.pinadmin.com>
2018-09-25 11:45:38 -0700

    Added basic Procfile

--- src/js/index.js
+++ src/js/index.js
@@ -1,48 +1,106 @@
-var cols = 5;
-var rows = 5;
-var padding = {x: 50, y: 50};
-var divs = [];
-var t = 0.01;
+// Tobias Toft 2018
+var startX = 0;
+var startY = 0;
+var weight = 100;
+var width = 25;
+var startWidth;
+var startWeight;
+var startItal;
 
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  //frameRate(60);
-  fill(0);
-  stroke(255);
+$('.status-button')._.bind('click', function(e){
+  randomizeVars();
+});
 
-  createSpans();
+
+$$('.v-font')._.bind('touchstart', function(e){
+  startX = e.touches[0].pageX;
+  startY = e.touches[0].pageY;
+
+  var props = getProps(e.target);
+  startWidth = parseInt(props.width);
+  startWeight = parseInt(props.weight);
+  startItal = parseInt(props.ital);
+
+  this.classList.add('touching');
+});
+
+$$('.v-font')._.bind('touchend', function(e){
+  e.target.classList.remove('touching');
+});
+
+
+$$('.v-font')._.bind('touchmove', function(e){
+  e.preventDefault();
+
+  var diffX = e.touches[0].pageX - startX;
+  var diffY = (e.touches[0].pageY - startY) * 3;
+  //diffX = _.clamp(diffX, 0, 175);
+  //diffY = _.clamp(diffY*3, 0, 800);
+
+  weight = Math.round(_.clamp(startWeight + diffY, 100, 900));
+  width = Math.round(_.clamp(startWidth + diffX, 0, 200));
+  ital = Math.round(_.clamp(startWidth + diffX, 0, 100));
+
+  updateStatusBar(weight, width, ital);
+  setVars(e.target, weight, width, ital);
+});
+
+
+
+function setVars(elm, _weight, _width, _ital) {
+  elm._.style({
+    'font-variation-settings' : "'wght' " + _weight + ", 'wdth' " + _width + ", 'ital' " + _ital
+  })
 }
 
-function draw() {
-  for (var x=0; x<divs.length; x++){
-    for (var y=0; y<divs[x].length; y++){
-      var n = noise(x+t, y+t);
-      var weight = 100 + n * 800;
-      var ital = n * 100;
-      divs[x][y].elt.style.fontVariationSettings = '"wght" ' + weight + ', "ital" ' + ital;
-    }
+
+function updateStatusBar(weight, width, ital){
+  $('.val-wdth').textContent = width;
+  $('.val-wght').textContent = weight;
+  $('.val-ital').textContent = ital;
+}
+
+function getProps(elm){
+  var props = {
+    weight,
+    width,
+    ital: 0
   }
-  t+=0.1;
-}
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
-
-
-//--
-
-function createSpans(){
-  for (var c=0; c<cols; c++){
-    var divRow = [];
-    for (var r=0; r<rows; r++){
-      var x = r*padding.x;
-      var y = c*padding.y;
-      var div = createDiv('A').addClass('letter');
-      div.position(x+50, y+50);
-      divRow.push(div);
+  window.getComputedStyle(elm).fontVariationSettings.replace(/"/g, '').split(',').forEach(function(prop){
+    var p = prop.trim().split(' ');
+    if (p[0] === 'wght'){
+      props.weight = p[1];
+    } else if (p[0] === 'wdth') {
+      props.width = p[1];
+    } else if (p[0] === 'ital') {
+      props.ital = p[1];
     }
-    divs.push(divRow);
-  }
+  });
+
+  return props;
+}
+
+function randomizeVars(){
+  addAnimationClass(300);
+  $$('.v-font').forEach(function(elm){
+    var _weight = 100 + Math.random()*800;
+    var _width = 0 + Math.random()*200;
+    var _ital = 0 + Math.random()*200;
+
+    setVars(elm, _weight, _width, _ital)
+  });
+}
+
+function addAnimationClass(time){
+  $$('.v-font').forEach(function(elm){
+    elm.classList.add('animate');
+  });
+
+  setTimeout(function(elm){
+    $$('.v-font').forEach(function(elm){
+      elm.classList.remove('animate');
+    });
+  }, time);
 }
